41be4275f22e03f0b3b6f3bfdb60586157bccfec
Ttebas <Ttebas@domain.com>
2025-05-16 21:11:50 -0300

    tarefa m1 - staging

---
+++ Tarefa_1/build/CMakeFiles/Hello3D.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ttebas/Documents/Unisinos/comp_grafica/CGbase/Common/glad.c" "CMakeFiles/Hello3D.dir/Common/glad.c.o" "gcc" "CMakeFiles/Hello3D.dir/Common/glad.c.o.d"
+  "/home/ttebas/Documents/Unisinos/comp_grafica/CGbase/src/Hello3D.cpp" "CMakeFiles/Hello3D.dir/src/Hello3D.cpp.o" "gcc" "CMakeFiles/Hello3D.dir/src/Hello3D.cpp.o.d"
+  "" "Hello3D" "gcc" "CMakeFiles/Hello3D.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Tarefa_1/build/CMakeFiles/SpherePhong.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ttebas/Documents/Unisinos/comp_grafica/CGbase/Common/glad.c" "CMakeFiles/SpherePhong.dir/Common/glad.c.o" "gcc" "CMakeFiles/SpherePhong.dir/Common/glad.c.o.d"
+  "/home/ttebas/Documents/Unisinos/comp_grafica/CGbase/src/SpherePhong.cpp" "CMakeFiles/SpherePhong.dir/src/SpherePhong.cpp.o" "gcc" "CMakeFiles/SpherePhong.dir/src/SpherePhong.cpp.o.d"
+  "" "SpherePhong" "gcc" "CMakeFiles/SpherePhong.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
